b6448da9ff6b077aedaedcc2d381560509d3d490
Daniel R. Reynolds <reynolds@smu.edu>
2022-08-01 17:20:47 -0500

    Added initialization of outputs in case of Newton solver failure

--- solve_DIRK.m
+++ solve_DIRK.m
@@ -228,6 +228,8 @@ function [y,yerr,cfail,lits] = DIRKstep_embedded(fcn, Jfcn, y0, t0, h, B)
 
    % initialize storage for RHS vectors, outputs
    k = zeros(length(y0),s);
+   y = y0;
+   yerr = zeros(size(y0));
    lits = 0;
    cfail = 0;
 
@@ -319,6 +321,7 @@ function [y,cfail,lits] = DIRKstep_basic(fcn, Jfcn, y0, t0, h, B)
 
    % initialize storage for RHS vectors, outputs
    k = zeros(length(y0),s);
+   y = y0;
    lits = 0;
    cfail = 0;
 
@@ -410,6 +413,8 @@ function [y,yerr,cfail,lits] = DIRKstep_Richardson(fcn, Jfcn, y0, t0, h, B)
 
    % initialize storage for RHS vectors, outputs
    k = zeros(length(y0),s);
+   y = y0;
+   yerr = zeros(size(y));
    lits = 0;
    cfail = 0;
 
